2749da53901d9ccbb1a8b347c087e2a68d6b91f7
huylg <lamgiahuy002203@gmail.com>
2024-12-11 09:02:41 +0700

    Hoàn thành bt1

---
+++ Session4-bt1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+	int n;
+	printf("Moi ban nhap mot so nguyen n: "); 
+	scanf("%d",&n); 
+	if( n > 0 ){
+		printf("Day la mot so duong \n"); 
+	}
+		else{
+			printf("Day la mot so am \n"); 
+		} 
+} 
